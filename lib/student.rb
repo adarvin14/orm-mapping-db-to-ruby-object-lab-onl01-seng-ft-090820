906a5a78b0fc4bc6643ccaf3199f3c95cf351ca3
Alex Darvin <amdarvin14@gmail.com>
2020-10-10 16:09:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,12 +71,11 @@ class Student
     sql = <<-SQL
       SELECT * FROM students
       WHERE grade = 10
-      ORDER BY students.id
-      LIMIT = 1
-    SQL
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end.first
+      LIMIT 1
+      SQL
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end.first
   end
   
   def self.all_students_in_grade_X(grade)
