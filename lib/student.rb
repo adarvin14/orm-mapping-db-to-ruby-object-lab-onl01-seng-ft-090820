9ff890aa7de2f3afa0428fb45b34543ba5c71543
Alex Darvin <amdarvin14@gmail.com>
2020-10-10 15:58:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,8 +57,7 @@ class Student
   
   def self.first_x_students_in_grade_10(number)
     sql = <<-SQL
-      SELECT *
-      FROM students
+      SELECT * FROM students
       WHERE grade = 10
       ORDER BY students.id
       LIMIT ?
