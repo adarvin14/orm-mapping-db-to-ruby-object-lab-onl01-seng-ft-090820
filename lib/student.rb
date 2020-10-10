841ebaadf6bca011b6afb906d5301f0047799087
Alex Darvin <amdarvin14@gmail.com>
2020-10-10 16:04:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,7 +67,7 @@ class Student
     end
   end
   
-  def self.all_studets_in_grade_X(grade)
+  def self.all_students_in_grade_X(grade)
     sql = <<-SQL
       SELECT * FROM students
       WHERE grade = ?
